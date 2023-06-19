e7d1ee0c3547b2dedfcf06ccb5691cd38101a692
JustinKrogel <122933595+JustinKrogel@users.noreply.github.com>
2023-06-19 15:46:57 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -43,10 +43,16 @@ return (
 			<img style = {{flex:1, marginLeft: 50, width: '35%', borderRadius: 30}} src = {portrait} alt="React Image" />
 			<div style = {{flex: 3}}>
 				<h1>
-					Dr. Krogel
+					Dr. JulieAnn Krogel
 				</h1>
-				<p>
-					
+				<p style = {{textAlign: 'Left', marginLeft: 80, marginRight: 80}}>
+				Are you ready to make a personal break through and move forward in your life? Do feel lost or stuck with your problems? Do you feel so overwhelmed and triggered sometimes that it is hard to do the things you know you should? We all get stuck sometimes and can benefit from expert professional help so we can heal from the negative experiences that hold us back and take charge of our lives.
+				</p>
+				<p style = {{textAlign: 'Left', marginLeft: 80, marginRight: 80}}>
+I would like to introduce you to the powerful healing method of Eye Movement Desensitization and Reprocessing Therapy (EMDR). Our lives are shaped by our experiences and negative life experiences can have a very damaging a lasting effect on how we feel, thing, and act. Nearly everyone goes through emotionally scaring life experiences at some point. Some common examples of these are experiences of abandonment, betrayal, humiliation, bullying, failure, abuse, and mortal danger. EMDR Therapy directly targets the neural networks where this learning is stored in the brain and helps it heal from these scaring events. Unlike traditional talk therapy that can take months, EMDR is direct and efficient and can resolve issues much more completely sometimes in just a few extended sessions.
+				</p>
+				<p style = {{textAlign: 'Left', marginLeft: 80, marginRight: 80}}>
+I am a specialist in EMDR Therapy with advanced training and years of clinical experience. I work with a wide range of mental health issues and have over 15 years of clinical experience. I would like to invite you to join me and learn to cope with the feelings that overwhelm you and get past the things that hold you back.
 				</p>
 			</div>
 		</div>
