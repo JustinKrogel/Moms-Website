dc043783f14a23e20c418d679458883e84e9d82a
JustinKrogel <122933595+JustinKrogel@users.noreply.github.com>
2023-08-04 16:22:31 -0400

    Add files via upload

---
+++ src/pages/Intensives.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PR from './pictures/Patient Registration Information Form-1.pdf';
+import PS from './pictures/Patient Services Agreement.pdf';
+import RI from './Release of Information.pdf';
+
+
+const Intensives = () => {
+return (
+	<div>
+		<div style = {{backgroundColor: '#e8ded1', marginTop: -25, height: 'calc(14vw)' }}>
+			<div style = {{height: 'calc(4vw)'}} />
+			<h1 style = {{textAlign: 'center', fontSize: 'calc(3vw)'}}>
+				Intensives
+			</h1>
+		</div>
+		<p style = {{marginLeft: 300, marginRight: 300}}>
+			Why spend weeks and months working through an emotional issue, when you could find resolution and relief in a matter of hours condensed into a couple of weeks? The traditional therapy format with Dr. Krogel and other therapists involves individuals attending 1 hour sessions every one to two weeks over a mater of weeks and months. Some individuals looking for more immediate relief or with constraints due to work schedules or driving distance may request to schedule Intensive sessions that last multiple hours and/or multiple days in a week. In this format months worth of therapy can be accomplish in a very short amount of time. This ability to condense the therapeutic work into a short amount of time is a special property of the EMDR Therapy that is not possible with most other psychotherapy approaches.
+
+
+		</p>
+
+
+
+
+	</div>
+
+	
+);
+};
+
+export default Intensives;
